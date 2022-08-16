2165966f4b03f9e39dbec89e355e2635b8bbabee
unknown <hamzarq999@gmail.com>
2022-08-16 05:21:36 -0700

    basic js

---
+++ Javascript/js/script.js
@@ -0,0 +1,26 @@
+function x (){
+    console.log("FUNCTION WORKED")
+    return
+}
+x ()
+
+
+//   TYPES 
+
+var a;
+console.log(a);
+
+if (a == undefined) {
+    console.log("a is undefined")
+}
+else {
+    console.log("a is defined")
+}
+
+//  FOR LOOP
+
+var sum = 0
+for (var i = 0; i < 10; i++){
+    sum += i
+}
+console.log("sum = ", sum)
